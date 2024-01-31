648ed006ebcec2d492061293ef05ea304de0b3c7
Fabiola Poblete <fabiola.broccolo@gmail.com>
2024-01-31 13:34:11 +0100

    Moved data file

--- packages/base/solarsystem/ui/index.tsx
+++ packages/base/solarsystem/ui/index.tsx
@@ -1,6 +1,16 @@
 export const SolarSystem = () => {
+  const url =
+    'https://github.com/fabiolapoblete/solaris/blob/main/packages/base/solarsystem/data/data.json';
 
-  const solarData = 
-  
+  async function fetchSolarData() {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error('error fetching json data', error);
+    }
+  }
+  fetchSolarData();
   return <section>yo</section>;
 };
